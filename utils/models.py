fa2a4323f1a485b210947b3aefa4a5a47b15f2b4
cuinuan <cuinuan@M1Pro.local>
2024-04-02 14:02:05 +0800

    add ResNet19 VGG19

--- utils/models.py
+++ utils/models.py
@@ -5,6 +5,31 @@ import torch.nn as nn
 from torchvision import models
 
 
+class ResNet18(nn.Module):
+    def __init__(self, num_classes=2):
+        super(ResNet18, self).__init__()
+        self.model = models.resnet18(weights=None)
+        num_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_features, num_classes)
+        self.name = "ResNet18"
+
+    def forward(self, x):
+        return self.model(x)
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+
 class ResNet50(nn.Module):
     def __init__(self, num_classes=2):
         super(ResNet50, self).__init__()
@@ -41,6 +66,34 @@ class VGG16(nn.Module):
         self.initialize_weights()
 
         self.name = "VGG16"
+        # self.target_layer = self.model.features[-1]
+
+    def forward(self, x):
+        return self.model(x)
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+
+class VGG19(nn.Module):
+    def __init__(self, num_classes=2):
+        super(VGG19, self).__init__()
+        self.model = models.vgg19(weights=None)
+        num_features = self.model.classifier[-1].in_features
+        self.model.classifier[-1] = nn.Linear(num_features, num_classes)
+        self.initialize_weights()
+
+        self.name = "VGG19"
         # self.target_layer = self.model.features[-1]
 
     def forward(self, x):
@@ -118,10 +171,14 @@ class AlexNet(nn.Module):
 def get_model(name: str):
     name = name.lower()
 
+    if name == "resnet18":
+        return ResNet18()
     if name == "resnet50":
         return ResNet50()
     elif name == "vgg16":
         return VGG16()
+    elif name == "vgg19":
+        return VGG19()
     elif name == "googlenet":
         return GoogLeNet()
     elif name == "alexnet":
